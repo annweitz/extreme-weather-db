33395be711efe7f0ddf4971b5e320f51fa6f945f
aweitz1 <annweitz@posteo.net>
2025-03-27 20:09:12 +0100

    Add general processor structure + processor for precipitation and temperature

---
+++ src/processing/processor.py
@@ -0,0 +1,193 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+import xarray as xr
+from databaseFunctions import createProcessingDatabase, createResultDatabase, insertEventsIntoDatabase
+from processing_functions import getConnectedEvents, update_top_n, getExistingTopTen
+from concurrent.futures import ThreadPoolExecutor
+
+PROCESSING_FOLDER = "./testProcessing/"
+PROCESSING_DB = f"{PROCESSING_FOLDER}processing.sql"
+RESULT_FOLDER = "./results/"
+RESULT_DB = f"{RESULT_FOLDER}results.sql"
+
+class ProcessingFactory:
+
+    processingFunctions = {}
+
+    @classmethod
+    def registerProcessor(cls, variableName, function):
+        cls.processingFunctions[variableName] = function
+
+    @classmethod
+    def getProcessor(cls, variableName):
+        function = cls.processingFunctions.get(variableName)
+        if not function:
+            raise ValueError(f"No processor found for variable {variableName}")
+        return function
+
+ProcessingFactory.registerProcessor("precipitation", lambda path: processPrecipitation(path))
+ProcessingFactory.registerProcessor("temperature", lambda path: processTemperature(path))
+
+
+
+
+def processPrecipitation(datasetPath):
+    # Define ptype values for rain and snow
+    rain_types = [1, 3, 7, 12]  # Rain, Freezing Rain, Rain-Snow Mix, Freezing Drizzle
+    snow_types = [5, 6, 8]      # Snow, Wet Snow, Ice Pellets
+
+    precipDataset = xr.open_dataset(datasetPath)
+
+    # preprocessing:
+    # - drop unsused vars: number, expver
+    precipDataset = precipDataset.drop_vars(["number","expver"])
+
+    # calculate a hourly dataset, that only contains rain precipitation
+    rainHourlyMask = precipDataset.ptype.isin(rain_types)
+    rainHourlyDS = precipDataset.copy()
+    rainHourlyDS['tp'] = precipDataset.tp.where(rainHourlyMask, 0)
+    rainHourlyDS = rainHourlyDS.drop_vars(["ptype"])
+
+    # calculate a hourly dataset, that only contains snow precipitation
+    snowHourlyMask = precipDataset.ptype.isin(snow_types)
+    snowHourlyDS = precipDataset.copy()
+    snowHourlyDS['tp'] = precipDataset.tp.where(snowHourlyMask, 0)
+    snowHourlyDS = snowHourlyDS.drop_vars(["ptype"])
+
+    # calculcate events where hourly rain exceeds 0.1m
+    rainHourlyThreshold = getConnectedEvents(rainHourlyDS, "tp", 0.1)
+
+    # Insert events into results database
+    insertEventsIntoDatabase(f"{RESULT_DB}", "rainHourly", rainHourlyThreshold)
+
+    # Drop ptype to save time and computational space, we don't need it for these calculations
+    precipDataset = precipDataset.drop_vars(["ptype"])
+    # Coarsen dataset to 24 hour intervals for daily processing with summed up precipitation values
+    precipDailyDS = precipDataset.coarsen(valid_time = 24).sum()
+    # If it exists, get current top 10 file
+    currentTopTenDS = getExistingTopTen(RESULT_FOLDER, "precipitation")
+
+    # Update top 10
+    precipDailyTop10 = update_top_n(precipDailyDS, "tp", oldTop10=currentTopTenDS)
+
+    if currentTopTenDS:
+        currentTopTenDS.close()
+
+    precipDailyTop10.to_netcdf(f"{RESULT_FOLDER}/top10precipitation.nc")
+    precipDailyTop10.close()
+
+    # Close both precipitation datasets, we dont need them after this
+    precipDailyDS.close()
+    precipDataset.close()
+
+    rainDailyDS  = rainHourlyDS.coarsen(valid_time = 24).sum()
+    rainDailyThreshold = getConnectedEvents(rainDailyDS, "tp", 0.2)
+
+    insertEventsIntoDatabase(f"{RESULT_DB}", "rainDaily", rainDailyThreshold)
+
+
+    rainHourlyDS.close()
+    rainDailyDS.close()
+
+    snowDailyDS = snowHourlyDS.coarsen(valid_time = 24).sum()
+    snowDailyThreshold = getConnectedEvents(snowDailyDS, "tp", 0.1)
+
+    insertEventsIntoDatabase(f"{RESULT_DB}", "snowDaily", snowDailyThreshold)
+
+    snowHourlyDS.close()
+    snowDailyDS.close()
+
+
+def processTemperature(datasetPath):
+
+    # open dataset
+    temperatureDS = xr.open_dataset(datasetPath)
+
+    # Preprocessing:
+    # - select pressure level
+    # - drop pressure level coordinate and useless number coordinate
+    temperatureDS = temperatureDS.isel(pressure_level = 0).drop_vars(["pressure_level", "number"])
+
+
+    # Get current top 10, if it already exists
+    currentTopTenDS = getExistingTopTen(RESULT_FOLDER, "temperatureHigh")
+
+    # Get top 10 highest values
+    temperatureTop10 = update_top_n(temperatureDS, "t",oldTop10= currentTopTenDS ,highest=True)
+
+    if currentTopTenDS:
+        currentTopTenDS.close()
+
+    # save and close
+    temperatureTop10.to_netcdf(f"{RESULT_FOLDER}/top10temperatureHigh.nc")
+    temperatureTop10.close()
+
+
+    # Get current top 10, if it already exists
+    currentTopTenDS = getExistingTopTen(RESULT_FOLDER, "temperatureLow")
+
+    # Get low 10
+    temperatureLow10 = update_top_n(temperatureDS, "t", oldTop10= currentTopTenDS, highest=False)
+
+    if currentTopTenDS:
+        currentTopTenDS.close()
+
+    # save and close
+    temperatureTop10.to_netcdf(f"{RESULT_FOLDER}/top10temperatureLow.nc")
+    temperatureTop10.close()
+
+
+    temperatureDS.close()
+
+def processingManager(arguments):
+
+    var,year = arguments.split(":")
+    year = int(year)
+
+    filepath = f"{PROCESSING_FOLDER}{var}_{year}.nc"
+
+    try:
+        processor = ProcessingFactory.getProcessor(var)
+        processor(filepath)
+    except ValueError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Unexpected error while processing variable {var} data: {e}")
+
+
+
+    pass
+def pack_records(records):
+    arguments = []
+    for row in records:
+        retVal = f"{row[0]}:{row[1]}"
+        arguments.append(retVal)
+    return arguments
+
+def main():
+    #print("test")
+    # establish sql connection to database
+    connection = sqlite3.connect(PROCESSING_DB)
+    cursor = connection.cursor()
+
+    exists = cursor.execute("Select exists(select 1 from sqlite_master where type = 'table' and name = 'processing')").fetchone()[0]
+    if exists == 0:
+        createProcessingDatabase(PROCESSING_FOLDER, PROCESSING_DB)
+
+    # get everything that has either not been done yet or failed
+    res = cursor.execute(f"select year,variable from processing where not status = 'processed' order by year desc")
+    records = res.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    arguments = pack_records(records)
+    createResultDatabase(RESULT_DB)
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.map(processingManager, arguments)
+
+
+if __name__ == "__main__":
+    main()
+
