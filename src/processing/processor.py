69b4d1ef252e35c61f22bcdb3b46d288ae1ff749
aweitz1 <annweitz@posteo.net>
2025-03-30 17:14:29 +0200

    Fix for config import

--- src/processing/processor.py
+++ src/processing/processor.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+project_root = Path(__file__).parent.parent.parent
+sys.path.append(str(project_root))
 import sqlite3
 from databaseFunctions import createProcessingDatabase, createResultDatabase, updateProcessingStatus, updateProcessingDatabase
 from concurrent.futures import ThreadPoolExecutor
