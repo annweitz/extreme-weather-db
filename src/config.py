1bd580d6062fd763c8ba027a26cfe8c8d68a749c
aweitz1 <annweitz@posteo.net>
2025-03-30 18:14:28 +0200

    Path fixes

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ PROJECT_ROOT = CONFIG_DIR.parent
 PROCESSING_FOLDER = "/projects/ag-schultz/"
 PROCESSING_DATABASE = f"{PROCESSING_FOLDER}processing.db"
 
-RESULT_FOLDER = PROJECT_ROOT.name + "/src/testResults/"
+RESULT_FOLDER = os.path.abspath(PROJECT_ROOT) + "/results/"
 RESULT_DATABASE = RESULT_FOLDER + "results.db"
 RESULT_TABLENAME = "thresholdResults"
 
